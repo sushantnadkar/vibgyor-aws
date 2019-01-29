dfdfa3ef95ddf0cbb071c109bdcafb4282a8a00e
Sushant Nadkar <nadkarsushant@gmail.com>
2019-01-29 13:58:13 +0530

    renamed data file

--- javascripts/site.js
+++ javascripts/site.js
@@ -2,13 +2,13 @@
 function loadJSON(callback) {   
 
   var xobj = new XMLHttpRequest();
-      xobj.overrideMimeType("application/json");
-  xobj.open('GET', 'https://sushantnadkar.github.io/vibgyoraws/data/upcommingevents.json', true); // Replace 'my_data' with the path to your file
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', 'https://sushantnadkar.github.io/vibgyoraws/data/data.js', true); // Replace 'my_data' with the path to your file
   xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-          // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-          callback(xobj.responseText);
-        }
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+      callback(xobj.responseText);
+    }
   };
   xobj.send(null);  
 }
